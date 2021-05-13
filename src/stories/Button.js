f4fed0628b2b8413fa98bc5b844b2c538702ad93
Robert Hilscher <r.hilscher@gmail.com>
2021-05-13 16:46:50 +0200

    add new props

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MaterialButton from '@material-ui/core/Button';
 
-export const Button = ({ backgroundColor, label, type, ...props }) => {
+export const Button = ({ backgroundColor, label, type, objectAbc, numberLabel, ...props }) => {
   return (
     <MaterialButton
       style={{ backgroundColor }}
       color={type}
       {...props}
     >
-      {label}
+      {label}-{numberLabel}-{objectAbc.name}
     </MaterialButton>
   );
 };
@@ -19,17 +19,19 @@ Button.propTypes = {
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   backgroundColor: PropTypes.string,
   label: PropTypes.string,
-  label2: PropTypes.number,
+  numberLabel: PropTypes.number,
   variant: PropTypes.oneOf(['text', 'outlined', 'contained']),
   disabled: PropTypes.bool,
+  objectAbc: PropTypes.shape({ name: PropTypes.string })
 };
 
 Button.defaultProps = {
   backgroundColor: null,
   label: 'Hello world',
-  label2: PropTypes.number,
+  numberLabel: 20,
   size: 'medium',
   variant: 'contained',
   type: 'primary',
   disabled: false,
+  objectAbc: { name: 'abc' }
 };
